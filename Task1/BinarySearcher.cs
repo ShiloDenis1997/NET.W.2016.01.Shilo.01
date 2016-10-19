bbd8dd012b3bf676f572d50323d3047a94a6d7ff
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-19 21:38:17 +0300

    Added generic binary search method realization

--- Task1/BinarySearcher.cs
+++ Task1/BinarySearcher.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
-    public class BinarySearcher
+    public static class BinarySearcher
     {
+        public static int BinarySearch<T>(T[] array, T x) where T : IComparable
+        {
+            int left = 0, right = array.Length, middle;
+            while (left + 1 < right)
+            {
+                middle = (left >> 1) + (right >> 1) + (1 & left & right);
+                if (array[middle].CompareTo(x) <= 0)
+                {
+                    left = middle;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+            if (array[left].CompareTo(x) == 0)
+            {
+                return left;
+            }
+            if (array[left].CompareTo(x) > 0)
+            {
+                return -left - 1;
+            }
+            return -left - 2;
+        }
     }
 }
