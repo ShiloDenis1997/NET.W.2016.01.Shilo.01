3a947da2f46b5bc4411c71cf4cfd0107e12c0d44
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-10-19 21:46:09 +0300

    Added console testing project

---
+++ Task1.ConsoleTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task1.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next();
+            Console.WriteLine(BinarySearcher.BinarySearch(array, 40));
+        }
+    }
+}
